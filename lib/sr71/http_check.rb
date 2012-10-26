d68c0d14064cb06b6601bf7043f453e64b7a9478
Michael Pilat <mike@mikepilat.com>
2012-10-26 16:34:42 -0500

    Add connect time to http_check logging.

--- lib/sr71/http_check.rb
+++ lib/sr71/http_check.rb
@@ -1,7 +1,30 @@
 module Sr71
   class HttpCheck
+    class ConnectionTimer
+      attr_reader :connect_time
+      def initialize(&block)
+        @callback = block
+      end
+
+      def request(client, head, body)
+        @callback.call
+        [head, body]
+      end
+
+      def response(resp)
+      end
+    end
+
     def self.check(url)
-      request = EventMachine::HttpRequest.new(url)
+      request = EventMachine::HttpRequest.new(url, {
+        :connect_timeout    => 5,
+        :inactivity_timeout => 30
+      })
+
+      connect_time = nil
+      request.use ConnectionTimer do
+        connect_time = Time.now
+      end
 
       start_time = Time.now
       http = request.get
@@ -11,8 +34,9 @@ module Sr71
         Sr71::Logger.log({
           :type => "http_check",
           :start_time => start_time,
+          :connect_time => connect_time - start_time,
           :elapsed_time => end_time - start_time,
-          :end_time => end_time,
+          :end_time => end_time.to_f,
           :url => url,
           :status_code => http.response_header.status
         })
@@ -24,7 +48,7 @@ module Sr71
           :type => "http_check",
           :start_time => start_time,
           :elapsed_time => end_time - start_time,
-          :end_time => end_time,
+          :end_time => end_time.to_f,
           :url => url,
           :error => http.error
         })
--- lib/sr71/logger.rb
+++ lib/sr71/logger.rb
@@ -3,7 +3,7 @@ require 'json'
 module Sr71
   class Logger
     def self.log(hash)
-      puts JSON.generate(hash)
+      puts JSON.generate(hash, :space => ' ', :object_nl => ' ')
     end
   end
 end
